98813062ae02e0969827e040b3b276bae4286f77
Vladislav Lepekha <vladislavlepekha@gmail.com>
2021-10-19 21:43:53 +0300

    project completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+function Footer() {
+    return (
+        <footer className="page-footer #e040fb purple accent-2">
+            <div className="footer-copyright">
+                <div className="container">
+                    © {new Date().getFullYear()} Copyright Text
+                    <a
+                        className="grey-text text-lighten-4 right"
+                        href="https://github.com/VladislavL-hub/react-shop-fortnite"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Reposit
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export { Footer };
---
+++ src/pages/About.jsx
@@ -0,0 +1,7 @@
+function About() {
+    return (
+        <h1>About pages</h1>
+    )
+}
+
+export {About};
